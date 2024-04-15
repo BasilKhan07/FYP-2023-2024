292408d6caa34df578884c183a5ec7e2bfacf2e5
BasilKhan07 <basilkhan06@gmail.com>
2024-04-15 19:56:22 +0500

    packages added

--- FYP II/intellicart/lib/views/customer/nav_screens/quality_screen.dart
+++ FYP II/intellicart/lib/views/customer/nav_screens/quality_screen.dart
@@ -1,18 +1,19 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:intellicart/api/quality_assessment.dart';
+// import 'package:intellicart/api/quality_assessment.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class QualityAssessmentScreen extends StatefulWidget {
-   const QualityAssessmentScreen({super.key});
+class ImagePredictionPage extends StatefulWidget {
+  const ImagePredictionPage({super.key});
 
   @override
-  State<QualityAssessmentScreen> createState() => _QualityAssessmentScreenState();
+  _ImagePredictionPageState createState() => _ImagePredictionPageState();
 }
 
-class _QualityAssessmentScreenState extends State<QualityAssessmentScreen> {
+class _ImagePredictionPageState extends State<ImagePredictionPage> {
   String _prediction = '';
-
   final picker = ImagePicker();
   File? _image;
 
@@ -34,20 +35,34 @@ class _QualityAssessmentScreenState extends State<QualityAssessmentScreen> {
     });
   }
 
-  predict() async {
-    if (_image != null) {
-      String predictionResult = await predictImage(_image);
-      setState(() {
-        _prediction = predictionResult;
-      });
-    } else {
-      setState(() {
-        _prediction = 'No image selected.';
-      });
-    }
+  Future predictImage() async {
+    if (_image == null) return;
+
+    String apiUrl = 'http://192.168.0.106:8001/predict/'; //use your PC's IP. go to ipconfig and then find your IP there.
+    // wifi on phone and laptop should be same 
+    var request = http.MultipartRequest('POST', Uri.parse(apiUrl));
+    request.files.add(await http.MultipartFile.fromPath('image', _image!.path));
+    var streamedResponse = await request.send();
+    print('AAA');
+    var response = await http.Response.fromStream(streamedResponse);
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> responseData = json.decode(response.body);
+      List<dynamic> predictions = responseData['predictions'];
+      if (predictions.isNotEmpty) {
+        setState(() {
+          _prediction = predictions.first;
+        });
+      } else {
+        setState(() {
+          _prediction = 'No prediction found';
+        });
+      }
+    } 
   }
 
- @override
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
@@ -77,7 +92,7 @@ class _QualityAssessmentScreenState extends State<QualityAssessmentScreen> {
             ],
           ),
           ElevatedButton(
-            onPressed: predict,
+            onPressed: predictImage,
             child: const Text('Predict'),
           ),
           const SizedBox(height: 20.0),
@@ -87,6 +102,91 @@ class _QualityAssessmentScreenState extends State<QualityAssessmentScreen> {
     );
   }
 }
+
+// class QualityAssessmentScreen extends StatefulWidget {
+//    const QualityAssessmentScreen({super.key});
+
+//   @override
+//   State<QualityAssessmentScreen> createState() => _QualityAssessmentScreenState();
+// }
+
+// class _QualityAssessmentScreenState extends State<QualityAssessmentScreen> {
+//   String _prediction = '';
+
+//   final picker = ImagePicker();
+//   File? _image;
+
+//   Future getImageFromGallery() async {
+//     final XFile? pickedFile = await picker.pickImage(source: ImageSource.gallery);
+//     _setImage(pickedFile);
+//   }
+
+//   Future getImageFromCamera() async {
+//     final XFile? pickedFile = await picker.pickImage(source: ImageSource.camera);
+//     _setImage(pickedFile);
+//   }
+
+//   void _setImage(XFile? pickedFile) {
+//     setState(() {
+//       if (pickedFile != null) {
+//         _image = File(pickedFile.path);
+//       } 
+//     });
+//   }
+
+//   predict() async {
+//     if (_image != null) {
+//       String predictionResult = await predictImage(_image);
+//       setState(() {
+//         _prediction = predictionResult;
+//       });
+//     } else {
+//       setState(() {
+//         _prediction = 'No image selected.';
+//       });
+//     }
+//   }
+
+//  @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: <Widget>[
+//           Container(
+//             constraints: BoxConstraints(
+//               maxWidth: MediaQuery.of(context).size.width * 0.8,
+//               maxHeight: MediaQuery.of(context).size.height * 0.4, 
+//             ),
+//             child: Center(
+//               child: _image == null ? const Text('No image selected.') : Image.file(_image!),
+//             ),
+//           ),
+//           const SizedBox(height: 20.0),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             children: [
+//               ElevatedButton(
+//                 onPressed: getImageFromGallery,
+//                 child: const Text('Select Image'),
+//               ),
+//               ElevatedButton(
+//                 onPressed: getImageFromCamera,
+//                 child: const Text('Take Picture'),
+//               ),
+//             ],
+//           ),
+//           ElevatedButton(
+//             onPressed: predict,
+//             child: const Text('Predict'),
+//           ),
+//           const SizedBox(height: 20.0),
+//           Text('Prediction: $_prediction'),
+//         ],
+//       ),
+//     );
+//   }
+// }
 
 // ignore_for_file: library_private_types_in_public_api
 
