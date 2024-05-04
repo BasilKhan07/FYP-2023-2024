d4712e24bb9a417ba61f1988ca1ac756107cf2e3
k200155 <alijodat16@gmail.com>
2024-05-04 05:12:39 +0500

    Images added in dashboard, and separate custom widget created

--- FYP II/intellicart/lib/views/vendor/nav_screens/product_screen.dart
+++ FYP II/intellicart/lib/views/vendor/nav_screens/product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intellicart/controllers/vendor_product_controller.dart';
+import 'widgets/product_display_card.dart';
 
 class ProductsScreen extends StatelessWidget {
   final VendorProductController _vendorProductController =
@@ -26,107 +27,9 @@ class ProductsScreen extends StatelessWidget {
 
             return Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Card(
-                  color: Color.fromARGB(255, 135, 218, 63).withOpacity(1),
-                  elevation: 4,
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 8,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          'Vegetables',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      vegList != null && vegList.isNotEmpty
-                      ? ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: vegList.length,
-                          itemBuilder: (context, index) {
-                            String productPrice = vegList[index]['price'].toStringAsFixed(2);
-                            String productName = vegList[index]['name'];
-                            return Container(
-                              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                              decoration: BoxDecoration(
-                                color: Color.fromARGB(255, 173, 217, 107).withOpacity(0.9), // Highlight color
-                                borderRadius: BorderRadius.circular(8.0), // Rounded corners
-                              ),
-                              child: ListTile(
-                                title: Text('$productName',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                subtitle: Text('Price: Rs. $productPrice per kg / dozen'),
-                              ),
-                            );
-                          },
-                        )
-                      : const ListTile(
-                          title: Text('No Vegetables listed for Sale'),
-                        ),
-                    ],
-                  ),
-                ),
-                Card(
-                  color: Color.fromARGB(255, 135, 218, 63).withOpacity(1),
-                  elevation: 4,
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 8,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          'Fruits',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      fruitList != null && fruitList.isNotEmpty
-                      ? ListView.builder(
-                          shrinkWrap: true,
-                          itemCount: fruitList.length,
-                          itemBuilder: (context, index) {
-                            String productPrice = fruitList[index]['price'].toStringAsFixed(2);
-                            String productName = fruitList[index]['name'];
-                            return Container(
-                              margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                              decoration: BoxDecoration(
-                                color: Color.fromARGB(255, 173, 217, 107).withOpacity(0.9), // Highlight color
-                                borderRadius: BorderRadius.circular(8.0), // Rounded corners
-                              ),
-                              child: ListTile(
-                                title: Text('$productName',
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                subtitle: Text('Price: Rs. $productPrice per kg / dozen'),
-                              ),
-                            );
-                          },
-                        )
-                      : const ListTile(
-                          title: Text('No Fruits listed for Sale'),
-                        ),
-                    ],
-                  ),
-                ),
+              children: [ 
+                CustomCategoryCard(categoryName: 'Vegetables',displayList: vegList, image: 'assets/images/vegetables.jpg'),
+                CustomCategoryCard(categoryName: 'Fruits',displayList: fruitList, image: 'assets/images/fruits.jpg'),
               ],
             );
           } //else
